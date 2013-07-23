f699d7026f4b05186f72eb36118d9c592e784fd4
DJ Jagannathan <desjag@gmail.com>
2013-07-22 22:02:08 -0400

    all the stuff so far

---
+++ innovate/scripts/home.js
@@ -0,0 +1,16 @@
+    function forgotPass()
+    {
+ 
+
+      if ( document.getElementById('recover').style.display == "none" ) {
+            document.getElementById('recover').style.display= "";
+          } else {
+            document.getElementById('recover').style.display = "none";
+          }    
+
+      if ( document.getElementById('enterForm').style.display == "" ) {
+            document.getElementById('enterForm').style.display= "none";
+          } else {
+            document.getElementById('enterForm').style.display = "";
+          }                                  
+    }
---
+++ innovate/scripts/profile.js
@@ -0,0 +1,73 @@
+    function showPort()
+    {
+ 
+
+      if ( document.getElementById('portfolioContent').style.display == "none" ) {
+            document.getElementById('portfolioContent').style.display= "";
+          } /*else {
+            document.getElementById('portfolioContent').style.display = "";
+          } */   
+
+      if ( document.getElementById('progressContent').style.display == "" ) {
+            document.getElementById('progressContent').style.display= "none";
+          } /*else {
+            document.getElementById('progressContent').style.display = "none";
+          }   */  
+
+      if ( document.getElementById('projectContent').style.display == "" ) {
+            document.getElementById('projectContent').style.display= "none";
+          } /*else {
+            document.getElementById('progressContent').style.display = "none";
+          }   */  
+
+      if ( document.getElementById('navPort').style.color !== "#B452CD" ){
+            document.getElementById('navPort').style.color == "#B452CD";
+      }                                          
+
+    }
+
+    function showProj()
+    {
+ 
+
+      if ( document.getElementById('projectContent').style.display == "none" ) {
+            document.getElementById('projectContent').style.display= "";
+          } /*else {
+            document.getElementById('portfolioContent').style.display = "";
+          } */   
+
+      if ( document.getElementById('progressContent').style.display == "" ) {
+            document.getElementById('progressContent').style.display= "none";
+          } /*else {
+            document.getElementById('progressContent').style.display = "none";
+          }   */  
+
+      if ( document.getElementById('portfolioContent').style.display == "" ) {
+            document.getElementById('portfolioContent').style.display= "none";
+          } /*else {
+            document.getElementById('progressContent').style.display = "none";
+          }   */                                         
+    }    
+
+    function showProg()
+    {
+ 
+
+      if ( document.getElementById('progressContent').style.display == "none" ) {
+            document.getElementById('progressContent').style.display= "";
+          } /*else {
+            document.getElementById('portfolioContent').style.display = "";
+          } */   
+
+      if ( document.getElementById('portfolioContent').style.display == "" ) {
+            document.getElementById('portfolioContent').style.display= "none";
+          } /*else {
+            document.getElementById('progressContent').style.display = "none";
+          }   */  
+
+      if ( document.getElementById('projectContent').style.display == "" ) {
+            document.getElementById('projectContent').style.display= "none";
+          } /*else {
+            document.getElementById('progressContent').style.display = "none";
+          }   */                                         
+    }    
