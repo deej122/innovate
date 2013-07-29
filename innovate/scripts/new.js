a4e6cd22c3ecce421ce0159074899e6dceb4919a
Deepak <kumarde@umich.edu>
2013-07-29 18:09:53 -0400

    added errors to new project

--- innovate/scripts/new.js
+++ innovate/scripts/new.js
@@ -1,9 +1,34 @@
 $(document).ready(function(){
 	$('#basicForm').submit(function(e){
-		return false;
+		e.preventDefault();
 	})
 	$('#formSubmit').click(function(){
 		var name = $('#projectName').val();
-		var tag = $('#projectTags').val();
+		var link = $('#pitchLink').val();
+		var teammates = $('#teammates').val();
+		var description = $('#description').val();
+		var errors = [];
+		if(name == ""){
+			errors.push("You need to enter a project name!");
+		} 
+		if(link == ""){
+			errors.push("You need to enter a link to your pitch!");
+		}
+		if(teammates == ""){
+			errors.push("You need to enter how many teammates you are looking for!");
+		}
+		if(description == ""){
+			errors.push("You need to enter a description of your project!")
+		}
+		if(errors.length == 0){
+			$('#basicForm').unbind("submit");
+			$('#basicForm').submit();
+		} else {
+			$('#errorContent').empty();
+        	for(var i = 0; i < errors.length; ++i){
+            	$('#errorContent').append("<p>"+errors[i]+"</p>");
+        	}
+        	$('#errors').modal('show');
+		}
 	})
 })
