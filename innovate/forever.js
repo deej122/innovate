42f37a36a5b4a370e007c57f03dac0a1e2f804b5
debug12 <debug12@gmail.com>
2013-10-11 19:35:22 -0400

    added start and stop forever scripts

---
+++ innovate/forever.js
@@ -0,0 +1,26 @@
+var forever = require('forever-monitor');
+
+var child = new(forever.Monitor)('app.js',{
+	max: 500,
+	silent: false,
+	watch: true,
+	killTree: true,
+	watchIgnoreDotFiles: null,
+	watchIgnorePatterns: null,
+	watchDirectory: __dirname,
+	options: [], //watch = true?,
+	logFile: './logs/log.out',
+	outFile: './logs/out.log',
+	errFile: './logs/err.log'
+});
+
+child.on('exit', function(){
+	console.log('app.js has exited after 3 restarts');
+});
+
+child.on('restart', function(){
+	console.log("restarted the server.");
+})
+
+
+child.start();
